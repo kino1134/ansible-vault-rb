6f8e694922d92e33592913b5198997171f1c8cd6
kino1134 <inoue1134@gmail.com>
2018-07-18 23:52:26 +0900

    改行コードをLFで固定にする

--- lib/ansible_vault.rb
+++ lib/ansible_vault.rb
@@ -18,8 +18,8 @@ module AnsibleVault
     # @return [String] 暗号化されたコンテキスト
     #   指定されたファイルがすでに暗号化されていた場合、空文字を返す
     def write(src, dest, password, label=nil, salt=nil)
-      cipher_text = encrypt(File.read(src, encoding: 'ascii-8bit'), password, label, salt)
-      File.write(dest, cipher_text, encoding: 'ascii-8bit') unless cipher_text.empty?
+      cipher_text = encrypt(File.read(src, encoding: 'ascii-8bit', mode: 'rt'), password, label, salt)
+      File.write(dest, cipher_text, encoding: 'ascii-8bit', mode: 'rt') unless cipher_text.empty?
       cipher_text
     end
 
